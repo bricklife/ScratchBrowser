0a79709fa3df92371b24c1396acd3c4ab0a3fa62
Shinichiro Oba <ooba@bricklife.com>
2019-11-15 23:19:29 +0900

    Update project for Xcode 11 and iOS 13

--- ScratchLink/Package.swift
+++ ScratchLink/Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "ScratchLink",
+    platforms: [
+        .iOS(.v11),
+    ],
     products: [
         .library(
             name: "ScratchLink",
